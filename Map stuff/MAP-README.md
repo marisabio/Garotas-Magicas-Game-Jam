57caba29b8274850a4928893dd1a1d50c746c94d
mari <mari.sabio@posteo.net>
2025-02-19 11:34:14 -0300

    map stuff

---
+++ Map stuff/MAP-README.md
@@ -0,0 +1,10 @@
+# MAP README
+
+0 - Black - Triggers
+1 - Yellow - Walls
+2 - Red - Doors
+3 - Green - Stairs up
+4 - Blue - Stairs down
+5 - Purple - Chests 
+
+Tile size tem que ser 3px X 3px durante a driação de mapas
---
+++ Map stuff/map-code-sprite-sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="map-code-sprite-sheet" tilewidth="3" tileheight="3" spacing="1" margin="1" tilecount="64" columns="8">
+ <image source="map-code-sprite-sheet.png" width="32" height="32"/>
+</tileset>
